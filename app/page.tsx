14b95e52d83dc9cbe1ef0e39c83afed8dfd844f6
Andrew Mandrichuk <andrew.mandrichuk2007@gmail.com>
2024-03-23 13:42:56 +0100

    chore: creating separate locales & filled components with starting code

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,31 @@
+import Header from "./home/Header";
+import Footer from "./home/Footer";
+import Gallery from "./home/Gallery";
+import Team from "./home/Team";
+import Prices from "./home/Prices";
+import Introduction from "./home/Introduction";
+import Newsletter from "./home/Newsletter";
+import SocialMedia from "./home/SocialMedia";
+import Activity from "./home/Activity";
 
-import React from 'react';
+import Image from "./components/Image";
 
 function Home() {
   return (
-    <div>
-      <p>Привет</p>
-    </div>
+    <main>
+
+      <Image image="77fitness/nqolbwafyj6u8pcltf8i" alt="77fitness" />
+
+      <Header />
+      <Introduction />
+      <Team />
+      <Gallery />
+      <SocialMedia />
+      <Prices />
+      <Activity />
+      <Newsletter />
+      <Footer />
+    </main>
   );
 }
 
