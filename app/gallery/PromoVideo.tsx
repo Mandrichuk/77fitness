91cfde94fe5f3c38739d2c0348da775dee1754ea
Andrew Mandrichuk <andrew.mandrichuk2007@gmail.com>
2024-03-30 16:34:31 +0100

    style: finalizing the style of the promo video section ::Gallery

--- app/gallery/PromoVideo.tsx
+++ app/gallery/PromoVideo.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { useTranslations } from "next-intl";
+
+import TextLayers from "../components/TextLayers";
 
 function PromoVideo() {
+  const t = useTranslations("GalleryPage.Promo");
+
   return (
-    <div>PromoVideo</div>
-  )
+    <section className="Gallery_PromoVideo">
+      <TextLayers bgText={t("bgText")} title={t("title")} />
+
+      <div className="wrapper">
+        <div className="video">Video Here!</div>
+      </div>
+    </section>
+  );
 }
 
-export default PromoVideo
+export default PromoVideo;
